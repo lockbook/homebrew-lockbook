9426db845f3159f5e9d152f2367a7277a5f60291
Parth <parth@mehrotra.me>
2020-07-08 20:14:24 -0400

    fix url

--- Formula/lockbook.rb
+++ Formula/lockbook.rb
@@ -1,9 +1,9 @@
 class Lockbook < Formula
   desc "Beautiful. Secure. Private. Everywhere."
   homepage "https://github.com/lockbook/lockbook"
-  url "https://github.com/lockbook/lockbook/releases/download/0.1/macos-cli.tar.gz"
-  sha256 "641588e5112df7b4afe5b7850d400e8a3d6da6a93bef095bb5c0f89108cc16f3"
-  version "0.1.0"
+  url "https://github.com/lockbook/lockbook/releases/download/0.1.1/macos-cli.tar.gz"
+  sha256 "7741cbc73d3315e3fbe7d87998ab8b3ee2b0670b1fd5f8cb7835d7297301a327"
+  version "0.1.1"
 
   def install
     bin.install "lockbook"
