1d95fa6045ebd6fd9a777ab9c549c18cf182d7fb
Parth <parth@mehrotra.me>
2020-07-08 20:21:27 -0400

    bump version

--- Formula/lockbook.rb
+++ Formula/lockbook.rb
@@ -3,7 +3,7 @@ class Lockbook < Formula
   homepage "https://github.com/lockbook/lockbook"
   url "https://github.com/lockbook/lockbook/releases/download/0.1.1/macos-cli.tar.gz"
   sha256 "7741cbc73d3315e3fbe7d87998ab8b3ee2b0670b1fd5f8cb7835d7297301a327"
-  version "0.1.1"
+  version "0.2.1"
 
   def install
     bin.install "lockbook"
