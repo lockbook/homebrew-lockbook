70ca3e6e68902e60cf6203c94a4fb5809887909e
Parth <parth@mehrotra.me>
2020-10-20 21:29:04 -0400

    Manual deploy by Parth from f16b47d6

--- Formula/lockbook.rb
+++ Formula/lockbook.rb
@@ -1,9 +1,9 @@
 class Lockbook < Formula
   desc "Beautiful. Secure. Private. Everywhere."
   homepage "https://github.com/lockbook/lockbook"
-  url "https://github.com/lockbook/lockbook/releases/download/0.2.14/lockbook-cli-macos.tar.gz"
-  sha256 "68440306a64069ec68121fb95038a7ade627c6905cf650d197b722a89f113205"
-  version "0.2.14"
+  url "https://github.com/lockbook/lockbook/releases/download/0.2.18/lockbook-cli-macos.tar.gz"
+  sha256 "442eef572d8786e8675fad9dd24c910feacdc4fec8396a9fb90439067b295ab4"
+  version "0.2.18"
 
   def install
     bin.install "lockbook"
