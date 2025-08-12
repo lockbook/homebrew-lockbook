f2c12100ffd132177c283851397d42fb91ac901e
Parth <parth@mehrotra.me>
2025-08-12 14:03:38 -0400

    releaser update

--- Formula/lockbook.rb
+++ Formula/lockbook.rb
@@ -2,9 +2,9 @@
 class Lockbook < Formula
   desc "The private, polished note-taking platform."
   homepage "https://github.com/lockbook/lockbook"
-  url "https://github.com/lockbook/lockbook/releases/download/0.9.25/lockbook-cli-macos.tar.gz"
-  sha256 "6f64563d826fba11dd555f414c418d80bb9336f3a7e9625d66b0b497f6ab6e67"
-  version "0.9.25"
+  url "https://github.com/lockbook/lockbook/releases/download/0.9.26/lockbook-cli-macos.tar.gz"
+  sha256 "e182df50999919c2a93747f99c879cd9c7c5b5cf7ebc63c722b42886963a3f34"
+  version "0.9.26"
 
   def install
     bin.install "lockbook"
